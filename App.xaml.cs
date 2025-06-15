b6686e3dac95d7f4411340ef4feadf631550cf2a
Zach Hammel <ojzach116@gmail.com>
2025-06-15 12:15:58 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using FinancialCalculator.ViewModel;
+
+namespace FinancialCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            MainWindow = new MainWindow() { DataContext = new MainWindowViewModel() };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+    }
+
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancialCalculator.ViewModel;
+
+namespace FinancialCalculator.ViewModel
+{
+    internal class MainWindowViewModel
+    {
+
+        public ViewModelBase CurrentViewModel { get; set; }
+
+
+        public MainWindowViewModel() 
+        { 
+            CurrentViewModel = new CalculatorViewModel();
+        }
+    }
+}
