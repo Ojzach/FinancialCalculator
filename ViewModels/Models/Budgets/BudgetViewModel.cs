7001296089e9f6bf642fd4d58974d87397571eba
Zach Hammel <ojzach116@gmail.com>
2025-10-04 17:57:54 -0400

    Feat (Edit Budget Menu): Added more base budget info to edit menu

--- ViewModels/Models/Budgets/BudgetViewModel.cs
+++ ViewModels/Models/Budgets/BudgetViewModel.cs
@@ -13,10 +13,12 @@ namespace FinancialCalculator.ViewModels
 
 
         public string BudgetName { get => _budget.Name; private set => _budget.Name = value; }
-
         public abstract string BudgetType { get; }
+        public string AssociatedFinancialAccountName { get => _budget.AssociatedFinancialAccount.accountName; }
 
         public float CurrentBalance { get => _budget.CurrentBudgetBalance; }
+
+        public string DebugInfo { get => "Recommended MD: $" + _budget.GetRecommendedMonthlyDepositAmt() + "  Min MD: $" + _budget.GetMinMonthlyDepositAmt() + "  Max MD: $" + _budget.GetMaxMonthlyDepositAmt();  }
 
         public BudgetViewModel(Budget budget)
         {
