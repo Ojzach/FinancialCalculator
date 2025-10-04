f76bc29b6034b4ea7eb35e85d409e22abfe4f92c
Zach Hammel <ojzach116@gmail.com>
2025-10-04 19:28:00 -0400

    Feat: Created new AmountPercentModel to replace my jank versions throughout the viewmodels

---
+++ Models/BudgetCalculatorDeposit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialCalculator.Models
+{
+    internal class BudgetCalculatorDeposit
+    {
+
+
+        public Budget DepositBudget { get; set; }
+        public int DepositAmount { get; set; }
+    }
+}
