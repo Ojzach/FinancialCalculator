696d20a5c87ec30388766e13353126c0da75f896
Zach Hammel <ojzach116@gmail.com>
2025-10-04 19:40:05 -0400

    Feat (IDFK): Im so confused with github. Idk where this came from so im just submitting it cause idc.

---
+++ Controls/AmtOrPctSetter.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinancialCalculator.Controls
+{
+    /// <summary>
+    /// Interaction logic for AmtOrPctSetter.xaml
+    /// </summary>
+    public partial class AmtOrPctSetter : UserControl
+    {
+
+
+        public bool IsSetByAmt { get => (bool)GetValue(IsSetByAmtProperty); set => SetValue(IsSetByAmtProperty, value); }
+
+        public float SetAmount { get => (float)GetValue(SetAmountProperty); set => SetValue(SetAmountProperty, value); }
+
+        public float SetPercent { get => (float)GetValue(SetPercentProperty); set => SetValue(SetPercentProperty, value); }
+
+        public string TitleText { get => (string)GetValue(TitleTextProperty); set => SetValue(TitleTextProperty, value); }
+
+
+        public static readonly DependencyProperty IsSetByAmtProperty =
+            DependencyProperty.Register(nameof(IsSetByAmt), typeof(bool), typeof(AmtOrPctSetter), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty SetAmountProperty =
+            DependencyProperty.Register(nameof(SetAmount), typeof(float), typeof(AmtOrPctSetter), new PropertyMetadata(0.0f));
+
+        public static readonly DependencyProperty SetPercentProperty =
+            DependencyProperty.Register(nameof(SetPercent), typeof(float), typeof(AmtOrPctSetter), new PropertyMetadata(0.0f));
+
+        public static readonly DependencyProperty TitleTextProperty =
+            DependencyProperty.Register(nameof(TitleText), typeof(string), typeof(AmtOrPctSetter), new PropertyMetadata(""));
+
+
+
+        public AmtOrPctSetter()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ResourceDictionaries/BudgetEditPanelTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using FinancialCalculator.Models;
+using FinancialCalculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FinancialCalculator.ResourceDictionaries
+{
+    internal class BudgetEditPanelTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+
+            if (element != null && item != null)
+            {
+
+                if (item is BudgetViewModel)
+                {
+                    if (item is SavingsBudgetViewModel) return element.FindResource("SavingsBudget_EditPanelTemplate") as DataTemplate;
+                    else if (item is FixedBudgetViewModel) return element.FindResource("FixedBudget_EditPanelTemplate") as DataTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
